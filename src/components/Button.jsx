2d3016276a466e243cce59a3d906b435bc14c82c
Esteban <marquezriosesteban@gmail.com>
2024-03-03 12:55:57 -0600

    Añadiendo bibliotecas externas hechas en React (Toastify)

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
+import {ToastContainer, toast} from 'react-toastify' //Uso de componentes externos hechos en React
 
 function Button() {
     const [subscribe, setSubscribe] = useState(false);
     return (
+        <>
         <button
-            onClick={() => setSubscribe(!subscribe)}
+            onClick={() => {
+                toast.success('Felicidades, ya estás suscrito');
+                setSubscribe(!subscribe);
+            }}
         >
             {subscribe ? "Ya estás suscrito" : 'Suscríbete'}
         </button>
+        <ToastContainer/>
+        </>
     )
 }
 
